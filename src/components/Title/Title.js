90e53cf7e1958660dd673873d675fb3d73083591
Mariana Faustino <marifaustino@gmail.com>
2023-02-08 18:49:18 -0300

    finalizando secao4

---
+++ src/components/Title/Title.js
@@ -0,0 +1,12 @@
+import styles from "./Title.module.css"
+import React from 'react'
+
+const Title = () => {
+  return (
+    <div>
+      <h2 className={styles.my_title}>Meu título</h2>
+    </div>
+  )
+}
+
+export default Title
